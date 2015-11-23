12c0d8b2ea1b518cd65d10495b8b8eb1187f58f4
Vladimir Letyago <vlad.letyago@gmail.com>
2015-11-23 13:24:33 +0300

    Added a message request  and other

--- client/client.go
+++ client/client.go
@@ -1,4 +1,4 @@
-package server
+package main
 
 import (
 	"../utils"
@@ -7,15 +7,27 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 const PORT = "7777"
 
+func main() {
+	// 1 Connect
+	conn, err := net.Dial("tcp", ":"+PORT)
+	utils.CheckError(err, "Can't connect to server", true)
+	client := NewClient(conn)
+	defer conn.Close()
+	//go
+	watchForConnection(client)
+}
+
+///////// Client Class ////////////////////////////////////////////////////////
+
 type Client struct {
 	uid      string
 	login    string
-	incoming chan string
-	outgoing chan string
+	outgoing chan []byte
 	reader   *bufio.Reader
 	writer   *bufio.Writer
 }
@@ -26,30 +38,28 @@ func NewClient(connection net.Conn) *Client {
 	client := &Client{
 		uid:      "",
 		login:    "",
-		incoming: make(chan string),
-		outgoing: make(chan string),
+		outgoing: make(chan []byte),
 		reader:   reader,
 		writer:   writer,
 	}
-	//client.Listen()
+	client.Listen()
 	return client
 }
 
-func main() {
-
-	// 1 Connect
-	conn, err := net.Dial("tcp", ":"+PORT)
-	utils.CheckError(err, "Can't connect to server", true)
-
-	client := NewClient(conn)
-
-	defer conn.Close()
-
-	//go
-	watchForConnection(client)
+// Listen - start corotinues for listening and writing
+func (c *Client) Listen() {
+	//go c.read()
+	go c.write()
 }
 
-func (c Client) Login() {
+func (c *Client) write() {
+	for data := range c.outgoing {
+		c.writer.Write(data)
+		c.writer.Flush()
+	}
+}
+
+func (c *Client) Login() {
 	s, err := json.Marshal(struct {
 		Action string        `json:"action"`
 		Data   utils.CltAuth `json:"data"`
@@ -64,11 +74,74 @@ func (c Client) Login() {
 	}
 }
 
-func (c Client) GetChannelList() {
+func (c *Client) Register() {
+	m := utils.CltRegister{
+		Nick: "владимир",
+	}
+	m.Login = "user"
+	m.Pass = "12345"
+
+	s, err := json.Marshal(struct {
+		Action string            `json:"action"`
+		Data   utils.CltRegister `json:"data"`
+	}{
+		Action: "register",
+		Data:   m,
+	})
+	c.login = "user"
+	c.uid = "user"
+	if utils.CheckError(err, "Register error", false) {
+		c.outgoing <- s
+	}
+}
+
+func (c *Client) GetChannelList() {
+}
+
+func (c *Client) Enter() {
+	m := utils.CltChannel{
+		Channel: utils.GetMD5Hash("Public"),
+	}
+	//m.sid = ""
+	//m.cid = ""
+
+	s, err := json.Marshal(struct {
+		Action string           `json:"action"`
+		Data   utils.CltChannel `json:"data"`
+	}{
+		Action: "enter",
+		Data:   m,
+	})
+
+	if utils.CheckError(err, "Register error", false) {
+		c.outgoing <- s
+	}
+}
+
+func (c *Client) Message() {
+	m := utils.CltMessage{
+		Body: "Текст на русском языке\nС переносом строк\n在中國文字",
+	}
+	m.Channel = utils.GetMD5Hash("Public")
+	//m.sid = ""
+	//m.cid = ""
+
+	s, err := json.Marshal(struct {
+		Action string           `json:"action"`
+		Data   utils.CltMessage `json:"data"`
+	}{
+		Action: "message",
+		Data:   m,
+	})
+
+	if utils.CheckError(err, "Register error", false) {
+		c.outgoing <- s
+	}
 }
 
 func watchForConnection(client *Client) {
 	dec := json.NewDecoder(client.reader)
+	count := 1
 	for {
 		var m utils.SrvMessage
 		err := dec.Decode(&m)
@@ -77,21 +150,32 @@ func watchForConnection(client *Client) {
 		case "welcome":
 			fmt.Println("Welcome message")
 			client.Login()
-		case "auth", "register", "leave":
+			//client.Register()
+		case "auth": //, "register", "leave":
 			fmt.Printf("Message %v\n", m.Action)
 			var im utils.SrvStatusMessage
 			if err := json.Unmarshal(m.RawData, &im); err != nil {
 				log.Fatal(err)
 				return
 			}
-
 			if im.Status == 0 {
-				client.GetChannelList()
+				client.Enter()
 			} else {
 				fmt.Printf("Auth error - %v", im.Error)
 				client.Login()
 			}
-
+		case "enter":
+			fmt.Printf("enter count - %v\n", count)
+			client.Message()
+		case "ev_enter":
+			fmt.Printf("ev_enter count - %v\n", count)
+		case "ev_message":
+			if count < 10 {
+				time.Sleep(time.Second)
+				fmt.Printf("ev_message count - %v\n", count)
+				client.Message()
+				count += 1
+			}
 		case "channellist":
 			fmt.Printf("channellist Message\n")
 		default:
